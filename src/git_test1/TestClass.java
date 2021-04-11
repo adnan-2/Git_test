05b9e3529e8d4a2343ae241b0c572e79120e69ca
adnan-2 <82262608+adnan-2@users.noreply.github.com>
2021-04-11 22:52:04 +0200

    Version02

--- src/git_test1/TestClass.java
+++ src/git_test1/TestClass.java
@@ -4,6 +4,8 @@ public class TestClass {
 
 	public static void main(String[] args) {
 		System.out.println("Git ten merhaba");
+		System.out.println("Version 1");
+		System.out.println("My second commit");
 
 	}
 
