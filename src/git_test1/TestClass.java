79da24802131e08188d1d2d9cffa1cda21b18009
adnan-2 <82262608+adnan-2@users.noreply.github.com>
2021-04-12 00:07:10 +0200

    Update TestClass.java

--- src/git_test1/TestClass.java
+++ src/git_test1/TestClass.java
@@ -6,14 +6,13 @@ public class TestClass {
 		System.out.println("Git ten merhaba");
 		System.out.println("Version 1");
 		System.out.println("My second commit");
-
 		System.out.println("Yeni bir kod");
 		System.out.println("My third commit");
-		
 		System.out.println("yeni bir mesaj var");
 		System.out.println(" bunu guncelestir");
 		System.out.println("Uzak sunucundaki kodlar");
 		System.out.println("Uzak sunucundaki en son kodlar");
+		System.out.println("15 satıra yeni kod yazdım");
 		
 	}
 	
